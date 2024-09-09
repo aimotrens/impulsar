72054f1194aff23438c6fc42d1c41b46d88ed9f5
Timo Ahrends <T_Ahrends@t3a6.de>
2024-09-09 22:23:26 +0200

    Bessere Shell Completion

--- cmd/shell_completion_scripts/bash.sh
+++ cmd/shell_completion_scripts/bash.sh
@@ -1,7 +1,102 @@
-#/usr/bin/env bash
-_impulsar_completions()
-{
-  COMPREPLY=($(compgen -W "$(impulsar show-jobs)" "${COMP_WORDS[1]}"))
+# Get value after -f option
+impulsar_get_f_value() {
+  local args=("$@")  # All args as array
+  for (( i = 1; i < ${#args[@]}; i++ )); do
+    if [[ "${args[i-1]}" == "-f" && -n "${args[i]}" ]]; then
+      echo "${args[i]}"  # Return value after -f
+      return 0
+    fi
+  done
+  return 1  # -f not found
 }
 
-complete -F _impulsar_completions impulsar
+# Generate completion for impulsar jobs
+impulsar_get_jobs() {
+  local provided_opt_f=$1
+
+  local file_option="-f impulsar.yml"
+  if [[ -n "$provided_opt_f" ]]; then
+    file_option="-f $provided_opt_f"
+  fi
+
+  local jobs
+  jobs=$(impulsar show-jobs $file_option 2>/dev/null)
+  COMPREPLY=($(compgen -W "$jobs" -- "$cur"))
+}
+
+# Main completion function for impulsar
+_impulsar_completion() {
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+  opts="run gen show-jobs version"
+
+  case "${COMP_WORDS[1]}" in
+    run)
+      # If the current word starts with '-', complete with options for 'run'
+      if [[ "$cur" == -* ]]; then
+        COMPREPLY=($(compgen -W "-f -e -dryrun -dump-jobs" -- "$cur"))
+        return 0
+      fi
+
+      # Handle the -f option specifically for file completion
+      if [[ "$prev" == "-f" ]]; then
+        COMPREPLY=($(compgen -f -- "$cur"))
+        return 0
+      fi
+
+      # Handle job completion after other options
+      if [[ "${COMP_WORDS[*]}" =~ "-f" ]]; then
+        local f_value
+        f_value=$(impulsar_get_f_value "${COMP_WORDS[@]}")
+        impulsar_get_jobs "$f_value"
+      else
+        impulsar_get_jobs
+      fi
+      ;;
+    gen)
+      # If the current word starts with '-', complete with options for 'gen'
+      if [[ "$cur" == -* ]]; then
+        COMPREPLY=($(compgen -W "-o" -- "$cur"))
+        return 0
+      fi
+
+      # Handle the -o option specifically
+      if [[ "$prev" == "-o" ]]; then
+        COMPREPLY=($(compgen -f -- "$cur"))
+        return 0
+      fi
+
+      # Completion for the 'gen' subcommand
+      COMPREPLY=($(compgen -W "bash zsh powershell" -- "$cur"))
+      ;;
+    show-jobs)
+      # If the current word starts with '-', complete with options for 'run'
+      if [[ "$cur" == -* ]]; then
+        COMPREPLY=($(compgen -W "-f" -- "$cur"))
+        return 0
+      fi
+
+      # Handle the -f option specifically for file completion
+      if [[ "$prev" == "-f" ]]; then
+        COMPREPLY=($(compgen -f -- "$cur"))
+        return 0
+      fi
+
+      # Completion for the 'show-jobs' subcommand
+      COMPREPLY=($(compgen -W "-f" -- "$cur"))
+      ;;
+    version)
+      # No additional arguments for 'version'
+      ;;
+    *)
+      # General completion for the first argument (subcommand)
+      COMPREPLY=($(compgen -W "$opts" -- "$cur"))
+      ;;
+  esac
+}
+
+# Register the completion function
+complete -F _impulsar_completion impulsar
--- cmd/shell_completion_scripts/zsh.sh
+++ cmd/shell_completion_scripts/zsh.sh
@@ -1,9 +1,85 @@
 #compdef impulsar
 
-_impulsar_jobs() {
-  local jobs
-  jobs=($(impulsar show-jobs))
-  _describe 'jobs' jobs
+# Get value after -f option
+impulsar_get_f_value() {
+  local args=("$@")  # All args as array
+  for (( i = 1; i <= ${#args[@]}; i++ )); do
+    if [[ "${args[i-1]}" == "-f" && -n "${args[i]}" ]]; then
+      echo "${args[i]}" # Return value after -f
+      return 0
+    fi
+  done
+  return 1 # -f not found
 }
 
-compdef _impulsar_jobs impulsar
+# Generate completion for impulsar jobs
+impulsar_get_jobs() {
+  local provided_opt_f=$1
+
+  local file_option=(-f impulsar.yml)
+  if [[ -n "$provided_opt_f" ]]; then
+    file_option=(-f "$provided_opt_f")
+  fi
+
+  local jobs
+  jobs=("${(@f)$(impulsar show-jobs "${file_option[@]}" 2>/dev/null)}")
+  _describe 'job' jobs
+}
+
+# Main completion function for impulsar
+impulsar_completion() {
+  local curcontext="$curcontext" state line
+
+  _arguments -C \
+    '1:subcommand:->subcmds' \
+    '*::args:->args'
+
+  case $state in
+    (subcmds)
+      local -a subcmds
+      subcmds=(
+        'run:run a job'
+        'gen:generate shell completion'
+        'show-jobs:show available jobs'
+        'version:show version'
+      )
+      _describe 'subcommand' subcmds
+      ;;
+    (args)
+      case $line[1] in
+        (run)
+          local f_value
+          f_value=$(impulsar_get_f_value "${line[@]}")
+
+          _arguments -s -C \
+            '-f[impulsar file]:file:_files' \
+            '-e[set environment variable]:name=value:' \
+            '-dryrun[dryrun, only show execution plan]' \
+            '-dump-jobs[dump parsed jobs to impulsar-dump.yml]' \
+            '*::job:->jobs'
+
+          case $state in
+            (jobs)
+              impulsar_get_jobs $f_value
+              ;;
+          esac
+          ;;
+        (gen)
+          _arguments -s -C \
+            '-o[output completion file]:file:_files' \
+            '*::shell type:(bash zsh powershell)' \
+          ;;
+        (show-jobs)
+          _arguments -s -C \
+            '-f[impulsar file]:file:_files'
+          ;;
+        (version)
+          # No additional arguments for 'version'
+          ;;
+      esac
+      ;;
+  esac
+}
+
+# Register the completion function with compdef
+compdef impulsar_completion impulsar
